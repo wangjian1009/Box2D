6b0209a56a720ceb6d48cd389f5e7a9124c23753
wangjian1009 <wangjian1009@gmail.coom>
2014-04-28 14:36:29 +0800

    编译配置文件调整

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 product:=Box2D
 $(product).type:=lib
 $(product).depends:=curl
-$(product).c.sources:=$(shell find $(product-base)/Box2D/Box2D -name *.cpp)
+$(product).c.sources=$(shell find $(product-base)/Box2D/Box2D -name *.cpp)
 $(product).product.c.includes:=3rdTools/Box2D/Box2D 
 
 $(eval $(call product-def,$(product),))
