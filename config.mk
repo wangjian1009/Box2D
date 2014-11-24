c306fd424c89417c5e3f18afb227cb337faad9f7
wangjian1009 <wangjian1009@gmail.coom>
2014-11-24 16:09:56 +0800

    remove makefile

--- config.mk
+++
@@ -1,8 +0,0 @@
-product:=Box2D
-$(product).type:=lib
-$(product).depends:=curl
-$(product).c.sources:=$(shell find $(product-base)/Box2D/Box2D -name *.cpp)
-$(product).product.c.includes:=3rdTools/Box2D/Box2D 
-
-$(eval $(call product-def,$(product),))
-
--- makefile
+++
@@ -1,3 +0,0 @@
-target-product:=Box2D
-include ../../buildtools/repository.mk
-include ../../buildtools/product.mk
